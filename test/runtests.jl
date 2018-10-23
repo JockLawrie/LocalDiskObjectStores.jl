44a9fc1e7ee4ee358b9e5dc1bf59b94f29259a43
Jock Lawrie <jock.lawrie@gmail.com>
2018-10-23 16:33:39 +1100

    Added more tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,7 @@ using LocalDiskStores
 
 # Create store
 store = BucketStore("mystore", "/tmp/rootbucket", LocalDiskStore())
+@test listcontents(store) == nothing  # Store doesn't have read permission
 setpermission!(store, :bucket, Permission(false, true, false, false))
 setpermission!(store, :object, Permission(false, true, false, false))
 
@@ -53,22 +54,32 @@ rm("/tmp/rootbucket", recursive=true)
 ################################################################################
 # Store with unrestricted read/create/delete permission on buckets and objects
 
+# Create store
 store = BucketStore("mystore", "/tmp/rootbucket", LocalDiskStore())
 setpermission!(store, :bucket, Permission(true, true, true, true))
 setpermission!(store, :object, Permission(true, true, true, true))
 
+# Root bucket
 @test listcontents(store) == nothing   # Root bucket doesn't yet exist
 @test createbucket!(store) == nothing  # Success (returns nothing)
 @test isempty(listcontents(store))     # Root bucket is empty
 
+@test typeof(createbucket!(store, "../xxx")) == String  # Failed (returns error msg) because the bucket is outside the root bucket
+
+# Bucket root/xxx
 @test createbucket!(store, "xxx") == nothing              # Success (returns nothing)
 @test listcontents(store) == ["xxx"]                      # Lists the contents of the root bucket
 @test typeof(createbucket!(store, "xxx")) == String       # Failed (returns error msg) because the bucket already exists
+
+# Object root/xxx/myobject
 store["xxx/myobject"] = "My first object"                 # Success (returns value)
 @test listcontents(store, "xxx") == ["myobject"]          # Lists the contents of the xxx bucket
 @test listcontents(store, "xxx/myobject") == nothing      # Failed (returns nothing) because we can only list the contents of buckets, not objects
 @test String(store["xxx/myobject"]) == "My first object"  # Get myobject's value
 @test store["xxx/my_nonexistent_object"] == nothing       # True because the object does not exist
+
+store["xxx/yyy/newobject"] = "Containing bucket doesn't exist"  # Fails (returns error msg) because containing bucket doesn't exist
+@test !isobject(store, "xxx/yyy/newobject")
 
 @test createbucket!(store, "xxx/yyy") == nothing          # Success (returns nothing), bucket yyy created inside bucket xxx
 @test listcontents(store, "xxx") == ["myobject", "yyy"]   # Bucket xxx contains the object myobject and the bucket yyy
